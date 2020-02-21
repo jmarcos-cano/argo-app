cf63960e3491c0bf1fe34e0851df6c0b91c7781d
@jmarcos-cano <jmarcos.cano@gmail.com>
2020-02-21 13:15:39 -0600

    @jmarcos-cano - adds deadlock.sh

---
+++ deadlock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kubectl get apps -A
+
+
+#kubectl delete app -n argocd helm-argo-app-yaml
+
+#? I had a finalizer in the app, going in and editing that to - finalizer: [] allowed it to delete.
+
